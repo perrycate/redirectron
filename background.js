1f202ee106ccb7d55a12ce310d7555c5b0f6bfd7
Perry <thescratcherman@gmail.com>
2014-01-01 17:08:30 -0500

    removed some commented out code that was still affecting background.js? Investigate later.

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ function sayHello()
 {
 	 // syntax error here screws up something, commands not working after this point.
 	 // (if uncommented that is.)
+	 // this is also trying to find the current url.
 	chrome.tabs.query({active: true; currentWindow: true}
 	alert("The current url is: " + tabs[0].url);
 
@@ -11,10 +12,12 @@ function sayHello()
 
 // chrome.webNavigation.onDOMContentLoaded.addListener(sayHello);
 
+// finds the current url when the icon is clicked.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	alert("button clicked");
 	alert("Your url is: " + tab.url);
 
 
+	// Confirm that this code block ran succesfully, since I can't find where error
+	// messages are outputted.
 	alert("code succesfully run.");
 });
