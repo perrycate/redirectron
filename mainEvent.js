5e9d8324af1a9201d8b31a14d40c8aa2bddbb62c
Perry <thescratcherman@gmail.com>
2014-01-03 00:58:36 -0500

    cleaned up code, added better comments.

--- mainEvent.js
+++ mainEvent.js
@@ -3,19 +3,22 @@ var proxyUrl = "http://tommy-b.appspot.com/"; // maybe use a different proxy?
 function redirect(tab)
 {
 	var url = tab.url;
-	var redirectUrl; 
+	var redirectUrl = ""; 
 
-	if(url.indexOf(proxyUrl) == -1) // if the proxy is not already part of the url
+	if(url.indexOf(proxyUrl) == -1) // if the proxy filter is not already in the url
 	{
-		// removes the extraneous "http://" and prepends the proxy filter
-		url = url.slice(url.indexOf("://") + 3);
-		redirectUrl = proxyUrl + url;
+		// Prepends the proxy to the url, removing unecessary part of url. (protocol)
+		redirectUrl = url.replace(/.*:\/\//, proxyUrl);
 	}
 	else
 	{
-		// removes the proxy filter and prepends with "http://"
-		url = url.slice(proxyUrl.length);
-		redirectUrl = "http://" + url; // what about https, though?
+		// removes the proxy from the url
+		redirectUrl = url.replace(proxyUrl, "http://");
+		/*
+		   Note: adding the "https://" back is necessary to make the url readable by
+		 the browser. It was taken out in the first place to make the url readable 
+		 to the proxy server.
+		 */
 	}
 
 	// redirects to the modified url
