b1cfd42c2226562ba00117d2d76a36a22ce93654
Perry <thescratcherman@gmail.com>
2014-01-03 00:23:00 -0500

    forgot to add the actual changes with last commit. oops.

--- mainEvent.js
+++ mainEvent.js
@@ -1,17 +1,25 @@
-var proxyUrl = "http://tommy-b.appspot.com/"; // maybe use google translate instead?
+var proxyUrl = "http://tommy-b.appspot.com/"; // maybe use a different proxy?
 
 function redirect(tab)
 {
-	// gets the current url, formats it, and prepends the proxy to use.
 	var url = tab.url;
-	url = url.slice((url.indexOf("://") + 3));
-	var redirectUrl = proxyUrl + url
+	var redirectUrl; 
+
+	if(url.indexOf(proxyUrl) == -1) // if the proxy is not already part of the url
+	{
+		// removes the extraneous "http://" and prepends the proxy filter
+		url = url.slice(url.indexOf("://") + 3);
+		redirectUrl = proxyUrl + url;
+	}
+	else
+	{
+		// removes the proxy filter and prepends with "http://"
+		url = url.slice(proxyUrl.length);
+		redirectUrl = "http://" + url; // what about https, though?
+	}
 
 	// redirects to the modified url
 	chrome.tabs.update(tab.id, {url: redirectUrl});
 }
 
 chrome.browserAction.onClicked.addListener(redirect);
-// ? note to self: why does the redirect function work without being passed anything?
-// more research on passing functions required.
-
